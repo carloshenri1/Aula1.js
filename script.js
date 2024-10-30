4216cfa249619c59c4a56e22f89ac81b9e961a22
Carlos Henrique <111144329+carloshenri1@users.noreply.github.com>
2024-10-29 21:19:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+/* var nome = prompt("Digite seu nome")
+
+alert("Olá " + nome + " seja bem vindo!")
+
+alert(`Olá ${nome}, seja bem vindo`)
+
+ */
+
+/* var primeiroValor = Number(prompt ("Digite um número:"))
+var segundoValor = Number(prompt ("Digite outro número:"))
+var resultadoSoma = primeiroValor / segundoValor
+
+
+
+console.log(primeiroValor)
+console.log(segundoValor)
+alert(`O resultado da soma = ${resultadoSoma}`) */
+
+var nome = prompt("Digite seu nome")
+var idade = prompt("Digite sua idade")
+var curso = prompt("Digite seu curso")
+var profissao = prompt("Digite sua profissão")
+
+alert (
+`Seu nome é: ${nome}
+Sua Idade é: ${idade}
+Seu curso é: ${curso}
+Sua Profissão é: ${profissao}   
+`
+
+)
+
